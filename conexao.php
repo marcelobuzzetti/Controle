2b081f70cbeb788bbb1464a2480ea61ec7a5296e
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2015-09-27 21:45:24 -0300

    Retirada User e Password

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $pdo = new PDO("mysql:host=localhost;dbname=controle", "root", "apollo87"); 
+    $pdo = new PDO("mysql:host=localhost;dbname=controle", "usurio", "senha"); 
         if(!$pdo){
             die("Erro ao criar a conexão");
         }
