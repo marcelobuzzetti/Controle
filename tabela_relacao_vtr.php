fe73c69de345bf88432d1b1ab7ae2eb593aa1c65
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2015-10-01 17:46:11 -0300

    Refatorado

--- tabela_relacao_vtr.php
+++ tabela_relacao_vtr.php
@@ -3,7 +3,15 @@
 $contador1 = 1;
 include 'conexao.php';
 try {
-    $stmt = $pdo->prepare("SELECT * FROM viaturasrodando");
+    $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS  modelo, placa, motoristas.apelido AS apelido, nome_destino, odo_saida, acompanhante, data_saida, hora_saida, odo_retorno, data_retorno, hora_retorno
+                                            FROM percursos, viaturas, motoristas, marcas, modelos, destinos
+                                            WHERE data_retorno IS NULL 
+                                            AND percursos.id_motorista = motoristas.id_motorista
+                                            AND percursos.id_viatura = viaturas.id_viatura
+                                            AND viaturas.id_marca = marcas.id_marca
+                                            AND viaturas.id_modelo = modelos.id_modelo
+                                            AND percursos.id_destino = destinos.id_destino
+                                            ORDER BY id_percurso DESC");
     $executa = $stmt->execute();
 
     if ($executa) {
@@ -15,7 +23,7 @@ try {
                         <td>Motorista</td>
                         <td>Destino</td>
                         <td>Odômetro Saída</td>
-                        <td>Ch Vtr</td>
+                        <td>Acompanhante</td>
                         <td>Data Saída</td>
                         <td>Hora Saída</td>
                     </tr>";
@@ -23,11 +31,11 @@ try {
         while ($reg = $stmt->fetch(PDO::FETCH_OBJ)) { /* Para recuperar um ARRAY utilize PDO::FETCH_ASSOC */
             echo "<tr>";
             echo "<td>$contador1</td>";
-            echo "<td>" . $reg->viatura . "</td>";
+            echo "<td>" .$reg->marca . " - " . $reg->modelo . " - " . $reg->placa."</td>";
             echo "<td>" . $reg->apelido . "</td>";
-            echo "<td>" . $reg->destino . "</td>";
+            echo "<td>" . $reg->nome_destino . "</td>";
             echo "<td>" . $reg->odo_saida . "</td>";
-            echo "<td>" . $reg->ch_vtr . "</td>";
+            echo "<td>" . $reg->acompanhante . "</td>";
             echo "<td>" . date('d M Y', strtotime($reg->data_saida)) . "</td>";
             echo "<td>" . $reg->hora_saida . "</td>";
             echo "</tr>";
