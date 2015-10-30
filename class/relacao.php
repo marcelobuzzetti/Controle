52e1650eefaaa9df967a550f02ae83b5d36f2e92
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2015-10-30 13:15:41 -0200

    Adicionado Classes

--- class/relacao.php
+++ class/relacao.php
@@ -394,4 +394,48 @@ class Relatorio{
                 }            
             }
 }
+
+class Usuario{
+    public function listarUsuario(){
+        include '../configs/conexao.php';
+          try {
+            $stmt = $pdo->prepare("SELECT id_usuario,login,descricao,nome 
+                                                FROM usuarios, perfis
+                                                WHERE cod_perfil = usuarios.id_perfil");
+            $executa = $stmt->execute();
+
+                if ($executa) {
+                    return $stmt->fetchAll(PDO::FETCH_ASSOC);                    
+                } else {
+                    print("<script language=JavaScript>
+                           alert('Não foi possível criar tabela.');
+                           </script>");
+                }
+                
+                } catch (PDOException $e) {
+                    echo $e->getMessage();                    
+                }            
+            }
+}
+
+class Perfil{
+    public function listarPerfil(){
+        include '../configs/conexao.php';
+          try {
+            $stmt = $pdo->prepare("SELECT * FROM perfis");
+            $executa = $stmt->execute();
+
+                if ($executa) {
+                    return $stmt->fetchAll(PDO::FETCH_ASSOC);                    
+                } else {
+                    print("<script language=JavaScript>
+                           alert('Não foi possível criar tabela.');
+                           </script>");
+                }
+                
+                } catch (PDOException $e) {
+                    echo $e->getMessage();                    
+                }            
+            }
+}
 ?>
