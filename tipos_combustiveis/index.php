bf372c2fa31a20319812e6a0010f9bec8e5dd02b
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2015-10-30 12:16:11 -0200

    Smarty

--- tipos_combustiveis/index.php
+++ tipos_combustiveis/index.php
@@ -1,38 +1,92 @@
-﻿<HTML>
-    <HEAD>
-        <TITLE>Controle de Entrada e Saída de Viaturas</TITLE>
-        <meta charset="UTF-8"/>
-        <script src="../js/jquery.js"></script>
-        <link   href="../css/bootstrap.css" rel="stylesheet">
-        <script src="../js/bootstrap.js"></script>
-        <script src="../js/script.js"></script>
-
-    </HEAD>
-    <BODY>
-        <?php
-        include "verificarLogin.php";
-        include"../menu.php";
-        include '../sessao.php';
-        ?>
-
-
-        <fieldset>
-            <legend>Cadastro Tipo de Combustivel</legend>
-            <table border=2px text-align='center' style='width: 40%'>
-                <form action="../executar.php" method="post">
-                    <tr>
-                        <td>Descrição</td>
-                        <td><label for="descricao"><input autofocus class="form-control" type="text" style='width: 150px' id="descricao" name="descricao" placeholder="Descrição" required="required"/></label></td>
-                    </tr>
-                    <td></td>
-                    <td><label><button type="submit" class="btn btn-primary" id="enviar" value="tipo" name="enviar">Cadastrar</button></label></td>
-                    </tr>
-                </form>
-            </table>
-        </fieldset>
-    </BODY>
-</HTML>
-
 <?php
-include 'tabela_tipo_cadastrados.php';
-?>
+session_start();
+
+if (!isset($_SESSION['login']) || ($_SESSION['perfil'] != 1 && $_SESSION['perfil'] != 3)) {
+    header('Location: ../percursos');
+} else {
+    require_once('../libs/smarty/Smarty.class.php');
+    include '../configs/sessao.php';
+    include '../configs/conexao.php';
+    include '../class/relacao.php';
+
+    $tipos_combustiveis = new TipoCombustivel();
+    $relacao_tipos_combustiveis = $tipos_combustiveis-> listarTiposCombustiveis();
+    
+    if(!isset($_POST['id'])){
+        
+        $smarty = new Smarty();
+        $smarty->assign('titulo', 'Cadastro de Tipo de Combustíveis');
+        $smarty->assign('botao', 'Cadastrar');
+        $smarty->assign('evento', 'tipo');
+        $smarty->assign('relacao_tipos_combustiveis', $relacao_tipos_combustiveis);
+        $smarty->assign('login', $_SESSION['login']);
+        $smarty->display('../templates/headers/header.tpl');
+
+        switch ($_SESSION['perfil']) {
+            case 1:
+                $smarty->display('../templates/menus/menuAdmin.tpl');
+                break;
+            case 2:
+                $smarty->display('../templates/menus/menuOperador.tpl');
+                break;
+            case 3:
+                $smarty->display('../templates/menus/menuMntGaragem.tpl');
+                break;
+            case 4:
+                $smarty->display('../templates/menus/menuMntS4.tpl');
+                break;
+            default:
+        }
+
+        $smarty->display('../templates/tipos_combustiveis/index.tpl');
+
+    } else {
+        $id = $_POST['id'];
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM tipos_combustiveis WHERE id_tipo_combustivel = ?");
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
+            $executa = $stmt->execute();
+            
+            if($executa){
+                $dados_tipos_combustiveis = $stmt->fetch(PDO::FETCH_OBJ);
+                $id_tipo_combustivel = $dados_tipos_combustiveis->id_tipo_combustivel;
+                $descricao = $dados_tipos_combustiveis->descricao;
+                
+            } else {
+                      print("<script language=JavaScript>
+                               alert('Não foi possível criar tabela.');
+                               </script>");                      
+            }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        
+        $smarty = new Smarty();
+        $smarty->assign('titulo', 'Atualização de Tipo de Combustíveis');
+        $smarty->assign('botao', 'Atualizar');
+        $smarty->assign('evento', 'atualizar_tipo');
+        $smarty->assign('id_tipo_combustivel', $id_tipo_combustivel);
+        $smarty->assign('descricao', $descricao);
+        $smarty->assign('relacao_tipos_combustiveis', $relacao_tipos_combustiveis);
+        $smarty->assign('login', $_SESSION['login']);
+        $smarty->display('../templates/headers/header.tpl');
+
+        switch ($_SESSION['perfil']) {
+            case 1:
+                $smarty->display('../templates/menus/menuAdmin.tpl');
+                break;
+            case 2:
+                $smarty->display('../templates/menus/menuOperador.tpl');
+                break;
+            case 3:
+                $smarty->display('../templates/menus/menuMntGaragem.tpl');
+                break;
+            case 4:
+                $smarty->display('../templates/menus/menuMntS4.tpl');
+                break;
+            default:
+        }
+
+        $smarty->display('../templates/tipos_combustiveis/index.tpl');
+    }
+}
