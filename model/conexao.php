c3b13716863ea7092282b87eb69f7faec54c8347
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2015-12-28 14:36:24 -0200

    Resolvendo Caracter Especial

--- model/conexao.php
+++ model/conexao.php
@@ -5,4 +5,17 @@ if (!$pdo) {
     die("Erro ao criar a conexão");
 }
 
+//Lidando com caracteres especiais
+$stmt = $pdo->prepare("SET NAMES 'utf8'");
+$executa = $stmt->execute();
+            
+$stmt = $pdo->prepare('SET character_set_connection=utf8');
+$executa = $stmt->execute();
+            
+$stmt = $pdo->prepare('SET character_set_client=utf8');
+$executa = $stmt->execute();
+            
+$stmt = $pdo->prepare('SET character_set_results=utf8');
+$executa = $stmt->execute();
+//Lidando com caracteres especiais
 ?>
