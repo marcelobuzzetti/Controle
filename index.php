02136eb6e3374b5fdd562210f0feffdfd80e563a
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-07 01:09:54 -0200

    Inserindo Classe Login

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <?php
 
-session_start();
-
-if ($_SESSION['erro'] == 1) {
-    echo "  <div class='alert alert-danger alert-dismissible' role='alert'>
-                            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-                            <strong>Usuário e/ou Senha não cadastrados</strong>
-                         </div>";
-}
-if ($_SESSION['timeout'] == 1) {
-    echo "  <div class='alert alert-danger alert-dismissible' role='alert'>
-                            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-                            <strong>Realizar o Login</strong>
-                         </div>";
-}
-session_destroy();
 include __DIR__.'/include/config.inc.php';
+
+$login = new Login();
+$login->Acesso();
 
 $viatura = new Viatura();
 $tabela_relacao_vtr = $viatura->ViaturasRodando();
