f87a9a2a8690c4068dd9fb21736e9713b688b9eb
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-07 01:10:12 -0200

    Criacao Classe Login e Sessao

---
+++ model/class/Sessao.class.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+class Sessao {
+
+    function tempo() {
+        if ($_SESSION['temposessao'] < time()) {
+            $_SESSION['timeout'] = 1;
+            header('Location: '.  constant("HOST"));
+        } else {
+            $_SESSION["temposessao"] = time() + 120;
+        }
+    }
+
+}
