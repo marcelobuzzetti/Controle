ae3bc7d85a078d0881bb82f37a4432464c9efa87
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-11 23:44:09 -0200

    Popular motorista de acordo com Vtr

---
+++ model/listaMotoristas.php
@@ -0,0 +1,32 @@
+<?php
+mysql_connect('localhost', 'controle', 'controle') or die('Erro ao conectar com o servidor');
+mysql_select_db('controle') or die('Erro ao conectar com o banco de dados');
+ mysql_query("SET NAMES 'utf8'");
+ mysql_query('SET character_set_connection=utf8');
+ mysql_query('SET character_set_client=utf8');
+ mysql_query('SET character_set_results=utf8');
+ 
+$id_viatura = $_GET['viatura'];
+$query = mysql_query("SELECT categoria
+FROM habilitacoes, viaturas, modelos
+WHERE viaturas.id_modelo = modelos.id_modelo
+AND modelos.id_habilitacao = habilitacoes.id_habilitacao
+AND viaturas.id_viatura = $id_viatura");
+$row = mysql_fetch_row($query);
+$categoria = $row[0];
+ 
+$rs = mysql_query("SELECT id_motorista, apelido FROM motoristas, habilitacoes
+    WHERE id_status != 2 
+    AND id_motorista NOT IN (SELECT id_motorista
+    FROM percursos 
+    WHERE data_retorno IS NULL)
+    AND motoristas.id_habilitacao = habilitacoes.id_habilitacao
+    AND categoria like '%$categoria%'");
+ 
+echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
+echo "<option value='' disabled selected>Selecione o Motorista</option>";
+while($reg = mysql_fetch_object($rs)){
+    echo "<option value=$reg->id_motorista>$reg->apelido</option>";
+}
+echo "</select>";
+?>
