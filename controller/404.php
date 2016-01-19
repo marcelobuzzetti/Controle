f85617de1dd3a98fceed9f226af32f87dccd8236
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-19 10:57:36 -0200

    Menu dinamico

--- controller/404.php
+++ controller/404.php
@@ -1,7 +1,12 @@
 <?php
 include '../include/config.inc.php';
+session_start();
+$menus = new Menu();
+$menu = $menus->SelecionarMenu($_SESSION['perfil']);
 
+$smarty->assign('login', $_SESSION['login']);
 $smarty->display('./headers/header.tpl');
+$smarty->display($menu);
 $smarty->display('./erros/404.tpl');
 $smarty->display('./footer/footer.tpl');
 
