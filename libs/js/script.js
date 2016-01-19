fc3fbbf65890ecada50820cbd0ad6a565c0e6126
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-19 10:59:28 -0200

    Inserindo autocompletar de modelos

--- libs/js/script.js
+++ libs/js/script.js
@@ -8,18 +8,44 @@ function imprimir() {
 }
 
 $(function () {
-    $('#data').datepicker({
-            showButtonPanel:true,
-            dateFormat: 'yy-mm-dd',
-        });
-    
-     $("#destino").autocomplete({
-                    source: "../model/buscador.php",
-                    minLength: 3
-                });
-                
-    $('#viatura').change(function(){
+    $('#data_inicio').datepicker({
+        showButtonPanel: true,
+        dateFormat: 'yy-mm-dd',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', '---', 'Sáb', 'Dom'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Mar&ccedil;o', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        currentText: 'Hoje',
+        nextText: 'Pr&oacute;ximo',
+        prevText: 'Anterior'
+    });
+
+    $('#data_fim').datepicker({
+        showButtonPanel: true,
+        dateFormat: 'yy-mm-dd',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', '---', 'Sáb', 'Dom'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Mar&ccedil;o', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        currentText: 'Hoje',
+        nextText: 'Pr&oacute;ximo',
+        prevText: 'Anterior'
+    });
+
+    $("#destino").autocomplete({
+        source: "../model/buscador.php",
+        minLength: 3
+    });
+
+    $('#viatura').change(function () {
         $('.Selecione').hide();
-        $('#motorista').load('../model/listaMotoristas.php?viatura='+$('#viatura').val());
-        });
+        $('#motorista').load('../model/listaMotoristas.php?viatura=' + $('#viatura').val());
+    });
+    
+    $('#marca').change(function () {
+        $('.Selecione').hide();
+        $('#modelo').load('../model/listaModelos.php?marca=' + $('#marca').val());
+    });
 });
