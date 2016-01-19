fd2be8281c6ac98bb085e12ec1daeba1087ba3eb
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-19 10:59:07 -0200

    Criando autocompletar de marca e modelo

---
+++ model/listaModelos.php
@@ -0,0 +1,19 @@
+<?php
+
+mysql_connect('localhost', 'controle', 'controle') or die('Erro ao conectar com o servidor');
+mysql_select_db('controle') or die('Erro ao conectar com o banco de dados');
+mysql_query("SET NAMES 'utf8'");
+mysql_query('SET character_set_connection=utf8');
+mysql_query('SET character_set_client=utf8');
+mysql_query('SET character_set_results=utf8');
+
+$id_marca = $_GET['marca'];
+
+    $rs = mysql_query("SELECT * FROM  modelos WHERE id_marca = $id_marca");
+    echo "<select class='form-control' name='modelo' id='modeo' required='required'>";
+    echo "<option value='' disabled selected>Selecione o Modelo</option>";
+    while ($reg = mysql_fetch_object($rs)) {
+        echo "<option value=$reg->id_modelo>$reg->descricao</option>";
+    }
+    echo "</select>";
+?>
