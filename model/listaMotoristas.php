70a728bf97990ceb23f601fbab37321eb64ff748
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-01-19 10:58:50 -0200

    LIstando somente disponiveis e escrevendo sem motorista

--- model/listaMotoristas.php
+++ model/listaMotoristas.php
@@ -1,32 +1,63 @@
 <?php
+
 mysql_connect('localhost', 'controle', 'controle') or die('Erro ao conectar com o servidor');
 mysql_select_db('controle') or die('Erro ao conectar com o banco de dados');
- mysql_query("SET NAMES 'utf8'");
- mysql_query('SET character_set_connection=utf8');
- mysql_query('SET character_set_client=utf8');
- mysql_query('SET character_set_results=utf8');
- 
+mysql_query("SET NAMES 'utf8'");
+mysql_query('SET character_set_connection=utf8');
+mysql_query('SET character_set_client=utf8');
+mysql_query('SET character_set_results=utf8');
+
 $id_viatura = $_GET['viatura'];
-$query = mysql_query("SELECT categoria
+$query = mysql_query("SELECT categoria, ordem
 FROM habilitacoes, viaturas, modelos
 WHERE viaturas.id_modelo = modelos.id_modelo
 AND modelos.id_habilitacao = habilitacoes.id_habilitacao
 AND viaturas.id_viatura = $id_viatura");
 $row = mysql_fetch_row($query);
 $categoria = $row[0];
- 
-$rs = mysql_query("SELECT id_motorista, apelido FROM motoristas, habilitacoes
+$ordem = $row[1];
+
+if ($categoria == "A") {
+    $rs = mysql_query("SELECT id_motorista, apelido FROM motoristas, habilitacoes
     WHERE id_status != 2 
     AND id_motorista NOT IN (SELECT id_motorista
     FROM percursos 
     WHERE data_retorno IS NULL)
     AND motoristas.id_habilitacao = habilitacoes.id_habilitacao
-    AND categoria like '%$categoria%'");
- 
-echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
-echo "<option value='' disabled selected>Selecione o Motorista</option>";
-while($reg = mysql_fetch_object($rs)){
-    echo "<option value=$reg->id_motorista>$reg->apelido</option>";
+    AND categoria like '%$categoria%'
+    AND id_motorista NOT IN (SELECT id_motorista FROM percursos WHERE data_retorno IS NULL)");
+    if (mysql_num_rows($rs) > 0) {
+        echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
+        echo "<option value='' disabled selected>Selecione o Motoristas</option>";
+        while ($reg = mysql_fetch_object($rs)) {
+            echo "<option value=$reg->id_motorista>$reg->apelido</option>";
+        }
+        echo "</select>";
+    } else {
+        echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
+        echo "<option value='' disabled selected>Sem Motoristas</option>";
+        echo "</select>";
+    }
+} else {
+    $rs = mysql_query("SELECT id_motorista, apelido FROM motoristas, habilitacoes
+    WHERE id_status != 2 
+    AND id_motorista NOT IN (SELECT id_motorista
+    FROM percursos 
+    WHERE data_retorno IS NULL)
+    AND motoristas.id_habilitacao = habilitacoes.id_habilitacao
+    AND (categoria like '%$categoria%' OR ordem >= $ordem)
+    AND id_motorista NOT IN (SELECT id_motorista FROM percursos WHERE data_retorno IS NULL)");
+    if (mysql_num_rows($rs) > 0) {
+        echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
+        echo "<option value='' disabled selected>Selecione o Motoristas</option>";
+        while ($reg = mysql_fetch_object($rs)) {
+            echo "<option value=$reg->id_motorista>$reg->apelido</option>";
+        }
+        echo "</select>";
+    } else {
+        echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
+        echo "<option value='' disabled selected>Sem Motoristas</option>";
+        echo "</select>";
+    }
 }
-echo "</select>";
 ?>
