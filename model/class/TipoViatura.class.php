1d244c2d61d7399b6c7424b38851a9c418276e9f
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-05 09:23:32 -0300

    Criacao

---
+++ model/class/TipoViatura.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class TipoViatura{ 
+    
+public function listarTiposViaturas(){
+        include '../model/conexao.php';
+          try {
+                $stmt = $pdo->prepare("SELECT * FROM tipos_viaturas");
+                $executa = $stmt->execute();
+
+                if ($executa) {
+                    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    print("<script language=JavaScript>
+                         alert('Não foi possível criar tabela tipo de viaturas.');
+                         </script>");
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+    }
+}
+
