378c228c852fcf58c500a91c5b4a170171a76f60
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-08 10:31:02 -0300

    Criacao Relatorio Vtr

--- model/class/Relatorio.class.php
+++ model/class/Relatorio.class.php
@@ -1,9 +1,10 @@
 <?php
 
-class Relatorio{
-    public function listarPercursos($inicio,$fim){
+class Relatorio {
+
+    public function listarPercursos($inicio, $fim) {
         include '../model/conexao.php';
-          try {
+        try {
             $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS modelo, placa, motoristas.apelido AS apelido, destinos.nome_destino AS destino, odo_saida, acompanhante, data_saida, hora_saida, odo_retorno, data_retorno, hora_retorno
                                                         FROM percursos, viaturas, motoristas, marcas, modelos, destinos
                                                         WHERE data_saida BETWEEN ? AND ?
@@ -16,17 +17,42 @@ class Relatorio{
             $stmt->bindParam(2, $fim, PDO::PARAM_STR);
             $executa = $stmt->execute();
 
-                if ($executa) {
-                    return $stmt->fetchAll(PDO::FETCH_ASSOC);                    
-                } else {
-                    print("<script language=JavaScript>
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                print("<script language=JavaScript>
                            alert('Não foi possível criar tabela.');
                            </script>");
-                }
-                
-                } catch (PDOException $e) {
-                    echo $e->getMessage();                    
-                }            
             }
-}
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
+    public function listarVtrUtilizacao($inicio, $fim) {
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT count(id_percurso) AS qnt, marcas.descricao AS marca, modelos.descricao AS modelo, placa, data_saida
+                                                        FROM percursos, viaturas, marcas, modelos
+                                                        WHERE data_saida BETWEEN ? AND ?
+                                                       AND percursos.id_viatura = viaturas.id_viatura
+                                                        AND viaturas.id_marca = marcas.id_marca
+                                                        AND viaturas.id_modelo = modelos.id_modelo 
+                                                        GROUP BY marca, modelo, placa");
+            $stmt->bindParam(1, $inicio, PDO::PARAM_STR);
+            $stmt->bindParam(2, $fim, PDO::PARAM_STR);
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                print("<script language=JavaScript>
+                           alert('Não foi possível criar tabela.');
+                           </script>");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+}
