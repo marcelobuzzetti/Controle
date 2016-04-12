f68789fb294c0bca2281fb287c87010f4ef3e3e8
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-12 19:13:23 -0300

    LIstar Status

---
+++ model/class/Status.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class Status{ 
+    public function listarStatus(){
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM status");
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                echo 'Erro ao inserir os dados';
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        }
+}
+
