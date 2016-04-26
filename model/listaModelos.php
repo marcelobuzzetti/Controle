4d4b9cd56c33a18201ae58bdcd8afdd6c699a19f
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-26 15:24:29 -0300

    Correcao

--- model/listaModelos.php
+++ model/listaModelos.php
@@ -9,11 +9,17 @@ mysql_query('SET character_set_results=utf8');
 
 $id_marca = $_GET['marca'];
 
-    $rs = mysql_query("SELECT * FROM  modelos WHERE id_marca = $id_marca");
+$rs = mysql_query("SELECT * FROM  modelos WHERE id_marca = $id_marca");
+if (mysql_num_rows($rs) > 0) {
     echo "<select class='form-control' name='modelo' id='modeo' required='required'>";
     echo "<option value='' disabled selected>Selecione o Modelo</option>";
     while ($reg = mysql_fetch_object($rs)) {
         echo "<option value=$reg->id_modelo>$reg->descricao</option>";
     }
     echo "</select>";
+} else {
+    echo "<select class='form-control' name='motorista' id='motorista' required='required'>";
+    echo "<option value='' disabled selected>Sem Modelos</option>";
+    echo "</select>";
+}
 ?>
