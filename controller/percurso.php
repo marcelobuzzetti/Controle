5ad0f6e5b0ebd8d7a5c61917169c838efd1f6557
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-25 21:12:39 -0300

    Alertas

--- controller/percurso.php
+++ controller/percurso.php
@@ -1,13 +1,14 @@
 <?php
+
 include '../include/config.inc.php';
 
 session_start();
 
 if (!isset($_SESSION['login'])) {
     session_unset();
-    header('Location: '.  constant("HOST"));
+    header('Location: ' . constant("HOST"));
 } else {
-    
+
     $viaturas = new Viatura();
     $tabela_relacao_viaturas = $viaturas->listarViaturasPercursos();
 
@@ -18,20 +19,26 @@ if (!isset($_SESSION['login'])) {
     $relacao_motoristas = $motoristas->listarMotoristas();
 
     $relacao_viaturas = $viaturas->listarViaturasPercursosDisponiveis();
-    
+
     $menus = new Menu();
     $menu = $menus->SelecionarMenu($_SESSION['perfil']);
-    
+
     $smarty->assign('titulo', 'Controle de Saída de Viatura');
     $smarty->assign('contador', $contador);
     $smarty->assign('tabela_relacao_vtr', $tabela_relacao_viaturas);
     $smarty->assign('tabela_relacao_vtr_fechadas', $tabela_relacao_viaturas_fechadas);
     $smarty->assign('relacao_motoristas', $relacao_motoristas);
     $smarty->assign('relacao_viaturas', $relacao_viaturas);
+    $smarty->assign('cadastrado', $_SESSION['cadastrado']);
+    $smarty->assign('atualizado', $_SESSION['atualizado']);
+    $smarty->assign('apagado', $_SESSION['apagado']);
     $smarty->assign('login', $_SESSION['login']);
     $smarty->display('./headers/header.tpl');
     $smarty->display($menu);
     $smarty->display('percurso.tpl');
     $smarty->display('./footer/footer.tpl');
+    unset($_SESSION['cadastrado']);
+    unset($_SESSION['atualizado']);
+    unset($_SESSION['apagado']);
 }
 ?>
