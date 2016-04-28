327ecb1f6732cfc1a2a3c04f1544797488a14343
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-28 11:34:27 -0300

    addslashes

--- model/listaMotoristas.php
+++ model/listaMotoristas.php
@@ -7,7 +7,7 @@ mysql_query('SET character_set_connection=utf8');
 mysql_query('SET character_set_client=utf8');
 mysql_query('SET character_set_results=utf8');
 
-$id_viatura = $_GET['viatura'];
+$id_viatura = addslashes($_GET['viatura']);
 $query = mysql_query("SELECT categoria, ordem
 FROM habilitacoes, viaturas, modelos
 WHERE viaturas.id_modelo = modelos.id_modelo
