101766962412571af79a0bb5fdc77b648f633516
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-29 10:41:48 -0300

    Group By

--- model/listaTipoCombustivel.php
+++ model/listaTipoCombustivel.php
@@ -6,7 +6,8 @@ $combustivel = $_GET['combustivel'];
 $stmt = $pdo->prepare("SELECT tc.id_tipo_combustivel AS id_tipo_combustivel, tc.descricao AS descricao
                                     FROM tipos_combustiveis tc, recibos_combustiveis rc 
                                     WHERE tc.id_tipo_combustivel = rc.id_tipo_combustivel 
-                                    AND rc.id_combustivel = ?");
+                                    AND rc.id_combustivel = ?
+                                    GROUP BY tc.id_tipo_combustivel");
 $stmt->bindParam(1, $combustivel, PDO::PARAM_INT);
 $executa = $stmt->execute();
 
