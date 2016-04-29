4b3018b3bb9a47df8ba28c7c32ef9f4094e5da3a
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-29 08:36:19 -0300

    Alerta do restante do combustível

---
+++ model/informaQntCombustivel.php
@@ -0,0 +1,36 @@
+<?php
+
+include '../model/conexao.php';
+
+$combustivel = $_GET['combustivel'];
+$tp = $_GET['tp'];
+$qnt = $_GET['qnt'];
+$stmt = $pdo->prepare("SELECT cr.combustivel AS combustivel, cr.tipo_combustivel AS tipo_combustivel, ((cr.qnt - ca.qnt) - ?) AS qnt
+                                    FROM combustivel_abastecido ca, combustivel_recebido cr, combustiveis c, tipos_combustiveis tc
+                                    WHERE cr.combustivel = ca.combustivel
+                                    AND cr.tipo_combustivel = ca.tipo_combustivel
+                                    AND c.descricao = ca.combustivel
+                                    AND tc.descricao = ca.tipo_combustivel
+                                    AND c.id_combustivel = ?
+                                    and tc.id_tipo_combustivel = ?
+                                    GROUP BY cr.combustivel, cr.tipo_combustivel");
+$stmt->bindParam(1, $qnt, PDO::PARAM_INT);
+$stmt->bindParam(2, $combustivel, PDO::PARAM_INT);
+$stmt->bindParam(3, $tp, PDO::PARAM_INT);
+$executa = $stmt->execute();
+
+if ($executa) {
+    $resultado = $stmt->fetch(PDO::FETCH_OBJ);
+    if ($resultado->qnt != NULL)
+        echo "<div class='alert alert-danger alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            Restam $resultado->qnt lts de $resultado->combustivel $resultado->tipo_combustivel
+        </div>";
+    else {
+        echo "<div class='alert alert-danger alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            Selecione o Combustível e o Tipo de Combustível
+        </div>";
+    }
+}
+?>
