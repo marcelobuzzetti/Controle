56d8d4b7490d3c67483b5065dc41dc70155fd74a
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-29 08:53:31 -0300

    Combustivel Disponivel

---
+++ controller/combustiveldisponivel.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../include/config.inc.php';
+
+session_start();
+if (!isset($_SESSION['login']) || ($_SESSION['perfil'] != 1 && $_SESSION['perfil'] != 3)) {
+    header('Location: ' . constant("HOST") . '/percurso');
+} else {
+
+    $existencia_combustivel = new CombustivelExistencia();
+    $tabela_existencia = $existencia_combustivel->listarCombustiveisExistentes();
+
+    $menus = new Menu();
+    $menu = $menus->SelecionarMenu($_SESSION['perfil']);
+
+    $smarty->assign('titulo', 'Existência de Combustível');
+    $smarty->assign('tabela_existencia', $tabela_existencia);
+    $smarty->display('./headers/header.tpl');
+    $smarty->display($menu);
+    $smarty->display('combustiveldisponivel.tpl');
+    $smarty->display('./footer/footer.tpl');
+}
