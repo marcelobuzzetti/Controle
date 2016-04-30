bccca1b0503ca0381cc2621ce0715f81e7ed5377
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-04-30 16:51:56 -0300

    Criacao

---
+++ model/odometroAbastecimento.php
@@ -0,0 +1,24 @@
+<?php
+
+include '../model/conexao.php';
+
+$id_viatura = $_GET['viatura'];
+$stmt = $pdo->prepare("SELECT MAX(odo_retorno) AS qnt FROM percursos WHERE id_viatura = ?");
+$stmt->bindParam(1, $id_viatura, PDO::PARAM_INT);
+$executa = $stmt->execute();
+
+if ($executa) {
+    $resultado = $stmt->fetch(PDO::FETCH_OBJ);
+    if ($resultado->qnt > 0) {
+        echo "<script> $('#odometro').attr('min','$resultado->qnt');</script>";
+    } else {
+        $stmt = $pdo->prepare("SELECT odometro FROM viaturas WHERE id_viatura = ?");
+        $stmt->bindParam(1, $id_viatura, PDO::PARAM_INT);
+        $executa = $stmt->execute();
+        if ($executa) {
+            $resultado = $stmt->fetch(PDO::FETCH_OBJ);
+            echo "<script> $('#odometro').attr('min','$resultado->odometro');</script>";
+        }
+    }
+}
+?>
