06d0bf2b0283abeba9cbb211e47c3be85801b369
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-05-15 10:31:04 -0300

    Se logado ir para inicio

--- model/class/Login.class.php
+++ model/class/Login.class.php
@@ -1,20 +1,26 @@
 <?php
+
 session_start();
+
 class Login {
-    function Acesso(){
-    if ($_SESSION['erro'] == 1) {
-    echo "  <div class='alert alert-danger alert-dismissible' role='alert'>
+
+    function Acesso() {
+        if (isset($_SESSION['login'])) {
+           header('Location: ' . constant("HOST").'/inicio');
+        } else {
+            if ($_SESSION['erro'] == 1) {
+                echo "  <div class='alert alert-danger alert-dismissible' role='alert'>
                             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
                             <strong>Usuário e/ou Senha não cadastrados</strong>
                          </div>";
-}
-if ($_SESSION['timeout'] == 1) {
-    echo "  <div class='alert alert-danger alert-dismissible' role='alert'>
+            }
+            if ($_SESSION['timeout'] == 1) {
+                echo "  <div class='alert alert-danger alert-dismissible' role='alert'>
                             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
                             <strong>Realizar o Login</strong>
                          </div>";
+            }
+            session_destroy();
+        }
+    }
 }
-    
-}
-}
-session_destroy();
