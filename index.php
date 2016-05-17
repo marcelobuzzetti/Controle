77687efef50712dcf3c26925890eab47d9ec00b9
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-05-17 11:20:26 -0300

    Exclusao

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $viatura = new Viatura();
 $tabela_relacao_vtr = $viatura->ViaturasRodando();
 
 $smarty->assign('tabela_relacao_vtr', $tabela_relacao_vtr);
-$smarty->assign('tabela_relacao_vtr', $tabela_relacao_vtr);
 $smarty->display('./headers/header_datatables.tpl');
 $smarty->display('home.tpl');
 $smarty->display('./footer/footer_datatables.tpl');
