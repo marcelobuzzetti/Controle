4263391af1a0bf9a5f7c08017392937c0f99f6ec
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-05-17 09:58:38 -0300

    ToDo

--- include/config.inc.php
+++ include/config.inc.php
@@ -9,4 +9,10 @@ include $_SERVER['DOCUMENT_ROOT'].'/model/conexao.php';
  $smarty->compile_dir = $_SERVER['DOCUMENT_ROOT'].'/templates_c/';
  $smarty->cache_dir = $_SERVER['DOCUMENT_ROOT'].'/cache/';
  $smarty->assign('HOST',  constant("HOST"));
+ 
+ /*Criar um contador de acesso de páginas do usuário
+ echo $url = $_SERVER['HTTP_HOST']. $_SERVER['REQUEST_URI'];
 
+ echo "<script>window.alert(window.location.href)</script>";
+  
+  */
