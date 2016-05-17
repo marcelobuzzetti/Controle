bd21486680aa9e70d09dbc3b8734c19137aaf52e
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-05-17 10:04:51 -0300

    Correcao

--- model/class/Sessao.class.php
+++ model/class/Sessao.class.php
@@ -5,12 +5,13 @@ session_start();
 class Sessao {
 
     function tempo() {
-        if ($_SESSION['temposessao'] < time()) {
-            $_SESSION['timeout'] = 1;
-            header('Location: '.  constant("HOST"));
-        } else {
-            $_SESSION["temposessao"] = time() + 120;
+        if ($_SESSION['temposessao']) {
+            if ($_SESSION['temposessao'] < time()) {
+                $_SESSION['timeout'] = 1;
+                header('Location: ' . constant("HOST"));
+            } else {
+                $_SESSION["temposessao"] = time() + 120;
+            }
         }
     }
-
 }
