e460a470ea630ad314ec24f094a2a127924e19c6
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-05-24 11:22:01 -0300

    Correcao

--- model/verificaViatura.php
+++ model/verificaViatura.php
@@ -17,15 +17,13 @@ $resultado = $stmt->fetch(PDO::FETCH_OBJ);
 $qnt = $resultado->qnt;
 
 if ($qnt > 0) {
-    echo "<div class='container'>
-        <div class='alert alert-danger alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+    echo "<div class='alert alert-danger alert-dismissible col-xs-12 col-sm-12 col-md-12'>
             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
             A viatura já está cadastrada
         </div>";
     echo "<script> $('#enviar').attr('disabled','disabled'), $('#marca').attr('style','border-color: red;'), $('#modelo').attr('style','border-color: red;'), $('#placa').attr('style','border-color: red;');</script>";
 } else {
-    echo "<div class='container'>
-        <div class='alert alert-success alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+    echo " <div class='alert alert-success alert-dismissible col-xs-12 col-sm-12 col-md-12'>
             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
            A viatura não está cadastrada
         </div>";
