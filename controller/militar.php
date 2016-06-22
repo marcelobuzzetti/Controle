9f4f9e073afdb9d4c77762fc0691e970ad773e05
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-06-21 21:43:05 -0300

    atualizar militares

--- controller/militar.php
+++ controller/militar.php
@@ -6,16 +6,16 @@ session_start();
 if (!isset($_SESSION['login']) || ($_SESSION['perfil'] == 2)) {
     header('Location: ' . constant("HOST") . '/percurso');
 } else {
- 
+
     $postograd = new PostoGrad();
     $relacao_posto_grad = $postograd->listarPostoGrad();
 
     $habiltacoes = new Habilitacao();
     $relacao_habilitacoes = $habiltacoes->listarHabilitacoes();
-    
+
     $militares = new Militar();
     $militares_cadastrados = $militares->listarMilitar();
-    
+
     $perfis = new Perfil();
     $relacao_perfis = $perfis->listarPerfil();
 
@@ -44,54 +44,50 @@ if (!isset($_SESSION['login']) || ($_SESSION['perfil'] == 2)) {
         unset($_SESSION['cadastrado']);
         unset($_SESSION['atualizado']);
         unset($_SESSION['apagado']);
-         unset($_SESSION['erro']);
+        unset($_SESSION['erro']);
     } else {
 
         $id = $_POST['id'];
+        $update = 1;
 
-        try {
-            $stmt = $pdo->prepare("SELECT militares.id_militar AS id_militar, id_posto_grad, militares.nome AS nome, nome_completo, DATE_FORMAT(data_nascimento,'%d/%m/%Y') AS data_nascimento, 
-                                                rg, orgao_expedidor, cpf, cnh, id_habilitacao, DATE_FORMAT(validade, '%d/%m/%Y') AS validade, id_perfil, login, usuarios.nome AS usuario, senha
-                                                FROM militares, motoristas, usuarios 
-                                                WHERE militares.id_militar = ?");
-            $stmt->bindParam(1, $id, PDO::PARAM_INT);
-            $executa = $stmt->execute();
-
-            if ($executa) {
-                $dados_militares = $stmt->fetch(PDO::FETCH_OBJ);
-                $id_militar = $dados_militares->id_militar;
-                $pg = $dados_militares->id_posto_grad;
-                $nome = $dados_militares->nome;
-                $nome_completo = $dados_militares->nome_completo;
-                $data_nascimento = $dados_militares->data_nascimento;
-                $rg = $dados_militares->rg;
-                $orgao_expedidor = $dados_militares->orgao_expedidor;
-                $cpf = $dados_militares->cpf;
-                
-            } else {
-                print("<script language=JavaScript>
-                   alert('Não foi possível criar tabela.');
-                   </script>");
-            }
-        } catch (PDOException $e) {
-            echo $e->getMessage();
+        $militar_atualizar = $militares->listarMilitarAtualizar($id);
+        $militar_atualizar_telefone = $militares->listarTelefoneMilitarAtualizar($id);
+        
+        if(empty($militar_atualizar_telefone) ){
+            $telefones = 0;
+        } else {
+            $telefones = 1;
         }
-
+        
+        
         $smarty->assign('titulo', 'Atualizar Militares');
         $smarty->assign('botao', 'Atualizar');
         $smarty->assign('evento', 'atualizar_militar');
-        $smarty->assign('id_militar', $id_militar);
-        $smarty->assign('id_pg', $pg);
-        $smarty->assign('categoria', $categoria);
-        $smarty->assign('nome', $nome);
-        $smarty->assign('nome_completo', $nome_completo);
-        $smarty->assign('data_nascimento', $data_nascimento);
-        $smarty->assign('rg', $rg);
-        $smarty->assign('orgao_expedidor', $orgao_expedidor);
-        $smarty->assign('cpf', $cpf);
-        $smarty->assign('relacao_posto_grad', $relacao_posto_grad);
-        $smarty->assign('relacao_habilitacoes', $relacao_habilitacoes);
+        $smarty->assign('update', $update);
+        $smarty->assign('telefones', $telefones);
+        $smarty->assign('id_militar', $militar_atualizar[0]['id_militar']);
+        $smarty->assign('numero_militar', $militar_atualizar[0]['numero_militar']);
+        $smarty->assign('cp', $militar_atualizar[0]['cp']);
+        $smarty->assign('grupo', $militar_atualizar[0]['grupo']);
+        $smarty->assign('antiguidade', $militar_atualizar[0]['antiguidade']);
+        $smarty->assign('data_praca', $militar_atualizar[0]['data_praca']);
+        $smarty->assign('id_pg', $militar_atualizar[0]['id_posto_grad']);
+        $smarty->assign('nome_completo', $militar_atualizar[0]['nome_completo']);
+        $smarty->assign('nome', $militar_atualizar[0]['nome']);
+        $smarty->assign('data_nascimento', $militar_atualizar[0]['data_nascimento']);
+        $smarty->assign('estado_nascimento', $militar_atualizar[0]['estado_nascimento']);
+        $smarty->assign('cidade_nascimento', $militar_atualizar[0]['cidade_nascimento']);
+        $smarty->assign('idt_militar', $militar_atualizar[0]['idt_militar']);
+        $smarty->assign('rg', $militar_atualizar[0]['rg']);
+        $smarty->assign('orgao_expedidor', $militar_atualizar[0]['orgao_expedidor']);
+        $smarty->assign('cpf', $militar_atualizar[0]['cpf']);
+        $smarty->assign('pai', $militar_atualizar[0]['pai']);
+        $smarty->assign('mae', $militar_atualizar[0]['mae']);
+        $smarty->assign('conjuge', $militar_atualizar[0]['conjuge']);
+        $smarty->assign('data_nascimento_conjuge', $militar_atualizar[0]['data_nascimento_conjuge']);
+        $smarty->assign('militar_atualizar_telefone', $militar_atualizar_telefone);
         $smarty->assign('militares_cadastrados', $militares_cadastrados);
+        $smarty->assign('relacao_posto_grad', $relacao_posto_grad);
         $smarty->assign('relacao_perfis', $relacao_perfis);
         $smarty->assign('login', $_SESSION['login']);
         $smarty->assign('cadastrado', $_SESSION['cadastrado']);
@@ -102,9 +98,9 @@ if (!isset($_SESSION['login']) || ($_SESSION['perfil'] == 2)) {
         $smarty->display($menu);
         $smarty->display('militar.tpl');
         $smarty->display('./footer/footer_datatables.tpl');
-         unset($_SESSION['cadastrado']);
+        unset($_SESSION['cadastrado']);
         unset($_SESSION['atualizado']);
         unset($_SESSION['apagado']);
-         unset($_SESSION['erro']);
+        unset($_SESSION['erro']);
     }
 }
