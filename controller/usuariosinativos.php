a0e92f6abd5437400c40e878d6b58fc1146c7e7f
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-06-28 14:36:51 -0300

    Criacao

---
+++ controller/usuariosinativos.php
@@ -0,0 +1,23 @@
+<?php
+
+include '../include/config.inc.php';
+
+session_start();
+if (!isset($_SESSION['login']) || ($_SESSION['perfil'] == 2)) {
+    header('Location: ' . constant("HOST") . '/percurso');
+} else {
+
+    $usuario = new Usuario();
+    $relacao_usuarios = $usuario->listarUsuarioInativos();
+
+    $menus = new Menu();
+    $menu = $menus->SelecionarMenu($_SESSION['perfil']);
+        
+        $smarty->assign('titulo', 'Usuários Inativos');
+        $smarty->assign('relacao_usuarios', $relacao_usuarios);
+        $smarty->assign('login', $_SESSION['login']);
+        $smarty->display('./headers/header_datatables.tpl');
+        $smarty->display($menu);
+        $smarty->display('usuariosinativos.tpl');
+        $smarty->display('./footer/footer_datatables.tpl');
+    }
