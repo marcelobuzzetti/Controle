0b067eab1b27fcc1fec3536c29be79f6d7a8e099
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-06-28 14:35:44 -0300

    MIlitares

--- model/class/Motorista.class.php
+++ model/class/Motorista.class.php
@@ -25,13 +25,39 @@ class Motorista{
     public function listarMotoristasCadastrados(){
         include '../model/conexao.php';
         try {
-            $stmt = $pdo->prepare("SELECT sigla, nome, nome_completo, cnh, DATE_FORMAT( validade,  '%d/%m/%Y' ) AS validade, categoria, status
+            $stmt = $pdo->prepare("SELECT  motoristas.id_motorista AS id_motorista, motoristas.id_militar AS id_militar,sigla, nome, nome_completo, cnh, DATE_FORMAT( validade,  '%d/%m/%Y' ) AS validade, categoria, status, motoristas.id_habilitacao AS id_habilitacao
                                                 FROM militares, posto_grad, 
-                                                STATUS , motoristas, habilitacoes
+                                                status , motoristas, habilitacoes
                                                 WHERE militares.id_status = status.id_status
                                                 AND militares.id_posto_grad = posto_grad.id_posto_grad
                                                 AND militares.id_militar = motoristas.id_militar
-                                                AND motoristas.id_habilitacao = habilitacoes.id_habilitacao");
+                                                AND motoristas.id_habilitacao = habilitacoes.id_habilitacao");       
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                
+                } else {
+                    print("<script language=JavaScript>
+                           alert('Não foi possível criar tabela.');
+                           </script>");
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }   
+    }
+    
+      public function listarMotoristasAtualizar($id){
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT motoristas.id_motorista AS id_motorista, motoristas.id_militar AS id_militar, sigla, nome, nome_completo, cnh, DATE_FORMAT( validade,  '%d/%m/%Y' ) AS validade, categoria, status, motoristas.id_habilitacao AS id_habilitacao
+                                                FROM militares, posto_grad, 
+                                                status , motoristas, habilitacoes
+                                                WHERE militares.id_status = status.id_status
+                                                AND militares.id_posto_grad = posto_grad.id_posto_grad
+                                                AND militares.id_militar = motoristas.id_militar
+                                                AND motoristas.id_habilitacao = habilitacoes.id_habilitacao
+                                                AND id_motorista = $id");
             $executa = $stmt->execute();
 
             if ($executa) {
