26c6c42c6c9846965eabd3f1659ada2079519352
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-06-28 14:54:08 -0300

    Correcao

--- model/class/Motorista.class.php
+++ model/class/Motorista.class.php
@@ -31,7 +31,34 @@ class Motorista{
                                                 WHERE militares.id_status = status.id_status
                                                 AND militares.id_posto_grad = posto_grad.id_posto_grad
                                                 AND militares.id_militar = motoristas.id_militar
-                                                AND motoristas.id_habilitacao = habilitacoes.id_habilitacao");       
+                                                AND motoristas.id_habilitacao = habilitacoes.id_habilitacao
+                                                AND motoristas.id_status = 1");       
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                
+                } else {
+                    print("<script language=JavaScript>
+                           alert('Não foi possível criar tabela.');
+                           </script>");
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }   
+    }
+    
+    public function listarMotoristasInativos(){
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT  motoristas.id_motorista AS id_motorista, motoristas.id_militar AS id_militar,sigla, nome, nome_completo, cnh, DATE_FORMAT( validade,  '%d/%m/%Y' ) AS validade, categoria, status, motoristas.id_habilitacao AS id_habilitacao
+                                                FROM militares, posto_grad, 
+                                                status , motoristas, habilitacoes
+                                                WHERE militares.id_status = status.id_status
+                                                AND militares.id_posto_grad = posto_grad.id_posto_grad
+                                                AND militares.id_militar = motoristas.id_militar
+                                                AND motoristas.id_habilitacao = habilitacoes.id_habilitacao
+                                                AND motoristas.id_status = 2");       
             $executa = $stmt->execute();
 
             if ($executa) {
