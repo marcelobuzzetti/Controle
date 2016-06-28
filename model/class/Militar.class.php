be83eae59834e139fa809b06199e2276e5cd77cb
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-06-28 14:35:53 -0300

    Motorista

--- model/class/Militar.class.php
+++ model/class/Militar.class.php
@@ -136,6 +136,29 @@ class Militar {
         }
     }
     
+    public function listarMilitarMotorista() {
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT id_militar, sigla, nome
+                                                FROM militares, posto_grad
+                                                WHERE militares.id_posto_grad = posto_grad.id_posto_grad                                                
+                                                AND militares.id_status !=2
+                                                AND militares.id_militar NOT IN (SELECT id_militar FROM motoristas)
+                                                ORDER BY antiguidade");
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                print("<script language=JavaScript>
+                           alert('Não foi possível criar tabela.');
+                           </script>");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    
      public function listarMilitarAtualizar($id) {
         include '../model/conexao.php';
         try {
