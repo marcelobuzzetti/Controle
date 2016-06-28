1af623c36fc9544cea575ebc9df8903d8b51fc42
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-06-28 14:53:13 -0300

    Criacao

---
+++ controller/motoristasinativos.php
@@ -0,0 +1,23 @@
+<?php
+
+include '../include/config.inc.php';
+
+session_start();
+if (!isset($_SESSION['login']) || ($_SESSION['perfil'] == 2)) {
+    header('Location: ' . constant("HOST") . '/percurso');
+} else {
+
+    $motoristas = new Motorista();
+    $tabela_motoristas_cadastrados = $motoristas->listarMotoristasInativos();
+
+    $menus = new Menu();
+    $menu = $menus->SelecionarMenu($_SESSION['perfil']);
+
+    $smarty->assign('titulo', 'Motoritas Inativos');
+    $smarty->assign('tabela_motoristas_cadastrados', $tabela_motoristas_cadastrados);
+    $smarty->assign('login', $_SESSION['login']);
+    $smarty->display('./headers/header_datatables.tpl');
+    $smarty->display($menu);
+    $smarty->display('motoristasinativos.tpl');
+    $smarty->display('./footer/footer_datatables.tpl');
+}
