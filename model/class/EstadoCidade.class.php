e848bd235d16ec8e0a406e50bffea44a3b08a499
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-01 10:31:02 -0300

    Criacao cidades e estados

---
+++ model/class/EstadoCidade.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class EstadoCidade{ 
+    public function listarEstado(){
+        include '../model/conexao.php';
+         try {
+                $stmt = $pdo->prepare("SELECT *
+                                                    FROM estados");
+                $executa = $stmt->execute();
+
+                if ($executa) {
+                    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    print("<script language=JavaScript>
+                         alert('Não foi possível criar tabela.');
+                         </script>");
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+    }
+    
+      public function listarCidades(){
+        include '../model/conexao.php';
+         try {
+                $stmt = $pdo->prepare("SELECT *
+                                                    FROM cidades");
+                $executa = $stmt->execute();
+
+                if ($executa) {
+                    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    print("<script language=JavaScript>
+                         alert('Não foi possível criar tabela.');
+                         </script>");
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+    }
+    }   
+
