0133af7d537338533a1102aa8b1270961d15b144
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-03 10:12:18 -0300

    Correcao menu

--- index.php
+++ index.php
@@ -5,11 +5,16 @@ include __DIR__.'/include/config.inc.php';
 $login = new Login();
 $login->Acesso();
 
+session_start();
+$menus = new Menu();
+$menu = $menus->SelecionarMenu($_SESSION['perfil']);
+
 $viatura = new Viatura();
 $tabela_relacao_vtr = $viatura->ViaturasRodando();
 
 $smarty->assign('tabela_relacao_vtr', $tabela_relacao_vtr);
 $smarty->display('./headers/header_datatables.tpl');
+$smarty->display($menu);
 $smarty->display('home.tpl');
 $smarty->display('./footer/footer_inicio.tpl');
 
