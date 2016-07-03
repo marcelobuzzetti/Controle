28dc1fdc134e6d99a08c1ff41fb8cf555908936d
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-03 12:19:44 -0300

    Correcao

--- model/verificaLogin.php
+++ model/verificaLogin.php
@@ -12,9 +12,28 @@ $resultado = $stmt->fetch(PDO::FETCH_OBJ);
 
 $qnt = $resultado->qnt;
 
-if ($qnt > 0) {
-    echo "<script>$('#usuario').attr('style','border:3px solid #7FFF00;');</script>";
+if ($login == NULL) {
+    echo "<div class='container'>
+        <div class='alert alert-danger alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            Preencha com um Login
+        </div>";
+    echo "<script> $('#enviar').attr('disabled','disabled'); $('#login').attr('style','border-color: red;');</script>";
 } else {
-    echo "<script> $('#usuario').attr('style','border-color: red;');</script>";
+    if ($qnt > 0) {
+        echo "<div class='container'>
+        <div class='alert alert-danger alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            O login $login já está cadastrado
+        </div>";
+        echo "<script> $('#enviar').attr('disabled','disabled'), $('#login').attr('style','border-color: red;');</script>";
+    } else {
+        echo "<div class='container'>
+        <div class='alert alert-success alert-dismissible col-xs-12 col-sm-12 col-md-12'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+            O login $login não está cadastrado
+        </div>";
+        echo "<script> $('#enviar').removeAttr('disabled'), $('#login').removeAttr('style');</script>";
+    }
 }
 ?>
