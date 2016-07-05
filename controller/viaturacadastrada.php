44413c941e65f7b4d7dbd56e34b939b552cdbf07
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-05 15:05:50 -0300

    $_SESSION

--- controller/viaturacadastrada.php
+++ controller/viaturacadastrada.php
@@ -4,27 +4,35 @@ include '../include/config.inc.php';
 
 session_start();
 if (isset($_SESSION['login']) == FALSE || ($_SESSION['perfil'] == 2 || $_SESSION['perfil'] == 5)) {
-header('Location: '. constant("HOST").'/percurso');
+    header('Location: ' . constant("HOST") . '/percurso');
 } else {
 
-$marcas = new Marca();
-$relacao_marcas = $marcas->listarMarcas();
+    $marcas = new Marca();
+    $relacao_marcas = $marcas->listarMarcas();
 
-$situacao = new Situacao();
-$relacao_situacao = $situacao->listarSituacao();
+    $situacao = new Situacao();
+    $relacao_situacao = $situacao->listarSituacao();
 
-$viaturas = new Viatura();
-$relacao_viaturas = $viaturas->listarViaturasCadastradas();
+    $viaturas = new Viatura();
+    $relacao_viaturas = $viaturas->listarViaturasCadastradas();
 
-$menus = new Menu();
-$menu = $menus->SelecionarMenu($_SESSION['perfil']);
+    $menus = new Menu();
+    $menu = $menus->SelecionarMenu($_SESSION['perfil']);
 
-$smarty->assign('titulo', 'Viaturas Cadastradas');
-$smarty->assign('relacao_viaturas', $relacao_viaturas);
-$smarty->assign('login', $_SESSION['login']);
-$smarty->display('./headers/header_datatables.tpl');
-$smarty->display($menu);
-$smarty->display('viaturacadastrada.tpl');
-$smarty->display('./footer/footer_datatables.tpl');
+    $smarty->assign('titulo', 'Viaturas Cadastradas');
+    $smarty->assign('relacao_viaturas', $relacao_viaturas);
+    $smarty->assign('login', $_SESSION['login']);
+    $smarty->assign('cadastrado', $_SESSION['cadastrado']);
+    $smarty->assign('atualizado', $_SESSION['atualizado']);
+    $smarty->assign('apagado', $_SESSION['apagado']);
+    $smarty->assign('erro', $_SESSION['erro']);
+    $smarty->display('./headers/header_datatables.tpl');
+    $smarty->display($menu);
+    $smarty->display('viaturacadastrada.tpl');
+    $smarty->display('./footer/footer_datatables.tpl');
+    unset($_SESSION['cadastrado']);
+    unset($_SESSION['atualizado']);
+    unset($_SESSION['apagado']);
+    unset($_SESSION['erro']);
 }
 ?>
