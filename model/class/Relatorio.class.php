0e7fe29f604b83770f5299d467ecf496ea3a6213
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-06 14:12:38 -0300

    Correcao

--- model/class/Relatorio.class.php
+++ model/class/Relatorio.class.php
@@ -5,17 +5,20 @@ class Relatorio {
     public function listarPercursos($inicio, $fim) {
         include '../model/conexao.php';
         try {
-            $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS modelo, placa, motoristas.apelido AS apelido, destinos.nome_destino AS destino, odo_saida, acompanhante, DATE_FORMAT(data_saida,'%d/%m/%Y') AS data_saida, hora_saida, odo_retorno, DATE_FORMAT(data_retorno,'%d/%m/%Y') AS data_retorno, hora_retorno
-                                                        FROM percursos, viaturas, motoristas, marcas, modelos, destinos
-                                                        WHERE data_saida BETWEEN ? AND ?
-                                                        OR data_retorno BETWEEN ? AND ?
-                                                        OR data_retorno IS NULL
-                                                        AND percursos.id_motorista = motoristas.id_motorista
-                                                        AND percursos.id_viatura = viaturas.id_viatura
-                                                        AND viaturas.id_marca = marcas.id_marca
-                                                        AND viaturas.id_modelo = modelos.id_modelo 
-                                                        AND percursos.id_destino = destinos.id_destino
-                                                        GROUP BY id_percurso");
+            $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, 
+                                                modelos.descricao AS modelo, placa, motoristas.apelido AS apelido, 
+                                                destinos.nome_destino AS destino, odo_saida, IFNULL(acompanhante,'Sem Acompanhantes') AS acompanhante, 
+                                                DATE_FORMAT(data_saida,'%d/%m/%Y') AS data_saida, hora_saida, odo_retorno, DATE_FORMAT(data_retorno,'%d/%m/%Y') AS data_retorno, hora_retorno
+                                                FROM percursos, viaturas, motoristas, marcas, modelos, destinos
+                                                WHERE data_saida BETWEEN ? AND ?
+                                                OR data_retorno BETWEEN ? AND ?
+                                                OR data_retorno IS NULL
+                                                AND percursos.id_motorista = motoristas.id_motorista
+                                                AND percursos.id_viatura = viaturas.id_viatura
+                                                AND viaturas.id_marca = marcas.id_marca
+                                                AND viaturas.id_modelo = modelos.id_modelo 
+                                                AND percursos.id_destino = destinos.id_destino
+                                                GROUP BY id_percurso");
             $stmt->bindParam(1, $inicio, PDO::PARAM_STR);
             $stmt->bindParam(2, $fim, PDO::PARAM_STR);
             $stmt->bindParam(3, $inicio, PDO::PARAM_STR);
@@ -37,7 +40,7 @@ class Relatorio {
      public function listarPercursosCompleto() {
         include '../model/conexao.php';
         try {
-            $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS modelo, placa, motoristas.apelido AS apelido, destinos.nome_destino AS destino, odo_saida, acompanhante, DATE_FORMAT(data_saida,'%d/%m/%Y') AS data_saida, hora_saida, odo_retorno, DATE_FORMAT(data_retorno,'%d/%m/%Y') AS data_retorno, hora_retorno
+            $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS modelo, placa, motoristas.apelido AS apelido, destinos.nome_destino AS destino, odo_saida, IFNULL(acompanhante,'Sem Acompanhantes') AS acompanhante, DATE_FORMAT(data_saida,'%d/%m/%Y') AS data_saida, hora_saida, odo_retorno, DATE_FORMAT(data_retorno,'%d/%m/%Y') AS data_retorno, hora_retorno
                                                 FROM percursos, viaturas, motoristas, marcas, modelos, destinos
                                                 WHERE percursos.id_motorista = motoristas.id_motorista
                                                 AND percursos.id_viatura = viaturas.id_viatura
