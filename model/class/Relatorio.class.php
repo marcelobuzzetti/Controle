b7eedd88262d3f75f91508449fc2cd44787628e2
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-06 13:22:15 -0300

    Mostrar vtr abertas

--- model/class/Relatorio.class.php
+++ model/class/Relatorio.class.php
@@ -8,13 +8,18 @@ class Relatorio {
             $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS modelo, placa, motoristas.apelido AS apelido, destinos.nome_destino AS destino, odo_saida, acompanhante, DATE_FORMAT(data_saida,'%d/%m/%Y') AS data_saida, hora_saida, odo_retorno, DATE_FORMAT(data_retorno,'%d/%m/%Y') AS data_retorno, hora_retorno
                                                         FROM percursos, viaturas, motoristas, marcas, modelos, destinos
                                                         WHERE data_saida BETWEEN ? AND ?
+                                                        OR data_retorno BETWEEN ? AND ?
+                                                        OR data_retorno IS NULL
                                                         AND percursos.id_motorista = motoristas.id_motorista
                                                         AND percursos.id_viatura = viaturas.id_viatura
                                                         AND viaturas.id_marca = marcas.id_marca
                                                         AND viaturas.id_modelo = modelos.id_modelo 
-                                                        AND percursos.id_destino = destinos.id_destino");
+                                                        AND percursos.id_destino = destinos.id_destino
+                                                        GROUP BY id_percurso");
             $stmt->bindParam(1, $inicio, PDO::PARAM_STR);
             $stmt->bindParam(2, $fim, PDO::PARAM_STR);
+            $stmt->bindParam(3, $inicio, PDO::PARAM_STR);
+            $stmt->bindParam(4, $fim, PDO::PARAM_STR);
             $executa = $stmt->execute();
 
             if ($executa) {
