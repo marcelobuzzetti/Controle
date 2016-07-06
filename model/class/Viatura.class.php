a49fda61e9cd3edf340f954ba30e77032c730e76
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-07-06 13:23:54 -0300

    Correcao

--- model/class/Viatura.class.php
+++ model/class/Viatura.class.php
@@ -130,5 +130,31 @@ class Viatura {
             echo $e->getMessage();
         }
     }
+    
+     public function ViaturasRodandoRelatorio() {
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT id_percurso, marcas.descricao AS marca, modelos.descricao AS  modelo, placa, motoristas.apelido AS apelido, nome_destino, odo_saida, acompanhante,  DATE_FORMAT(data_saida,'%d/%m/%Y') AS data_saida, hora_saida, odo_retorno,  DATE_FORMAT(data_retorno,'%d/%m/%Y') AS data_retorno, hora_retorno
+                                                       FROM percursos, viaturas, motoristas, marcas, modelos, destinos
+                                                       WHERE data_retorno IS NULL 
+                                                       AND percursos.id_motorista = motoristas.id_motorista
+                                                       AND percursos.id_viatura = viaturas.id_viatura
+                                                       AND viaturas.id_marca = marcas.id_marca
+                                                       AND viaturas.id_modelo = modelos.id_modelo
+                                                       AND percursos.id_destino = destinos.id_destino
+                                                       ORDER BY id_percurso DESC");
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                print("<script language=JavaScript>
+                              alert('Não foi possível criar tabela de Viaturas Rodando.');
+                              </script>");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
 }
