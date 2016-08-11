6dd5b3929d786600d18b10270088397b7d28dbef
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-08-11 14:14:35 -0300

    Criacao

---
+++ controller/alteracao_viatura.php
@@ -0,0 +1,84 @@
+<?php
+
+include '../include/config.inc.php';
+
+session_start();
+
+if (isset($_SESSION['login']) == FALSE || ($_SESSION['perfil'] != 1 && $_SESSION['perfil'] != 3 && $_SESSION['perfil'] != 4)) {
+    session_unset();
+    header('Location: ' . constant("HOST"));
+} else {
+
+    $viaturas = new Viatura();
+    $relacao_viaturas = $viaturas->listarViaturas();
+    
+    $alteracao = new AlteracaoViatura();
+    $relacao_alteracao = $alteracao->listarAlteracaoVtr();
+
+    $menus = new Menu();
+    $menu = $menus->SelecionarMenu($_SESSION['perfil']);
+
+    if (!isset($_POST['id'])) {
+
+        $smarty->assign('titulo', 'Alteração de Viaturas');
+        $smarty->assign('botao', 'Cadastrar');
+        $smarty->assign('evento', 'cadastrar_alteracao');
+        $smarty->assign('relacao_viaturas', $relacao_viaturas);
+        $smarty->assign('relacao_alteracao',  $relacao_alteracao);
+        $smarty->assign('cadastrado', $_SESSION['cadastrado']);
+        $smarty->assign('atualizado', $_SESSION['atualizado']);
+        $smarty->assign('apagado', $_SESSION['apagado']);
+        $smarty->assign('login', $_SESSION['login']);
+        $smarty->display('./headers/header_datatables.tpl');
+        $smarty->display($menu);
+        $smarty->display('alteracao_viatura.tpl');
+        $smarty->display('./footer/footer_datatables.tpl');
+        unset($_SESSION['cadastrado']);
+        unset($_SESSION['atualizado']);
+        unset($_SESSION['apagado']);
+    } else {
+        $id = $_POST['id'];
+        try {
+            $stmt = $pdo->prepare("SELECT id_alteracao_viatura, id_viatura, odometro, descricao, DATE_FORMAT(data,'%d/%m/%Y') AS data FROM alteracao_viaturas WHERE id_alteracao_viatura = ?");
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
+            $executa = $stmt->execute();
+            if ($executa) {
+                $dados_alteracao_viaturas = $stmt->fetch(PDO::FETCH_OBJ);
+                $id_alteracao_viatura = $dados_alteracao_viaturas ->id_alteracao_viatura;
+                $id_viatura = $dados_alteracao_viaturas ->id_viatura;
+                $odometro = $dados_alteracao_viaturas ->odometro;
+                $descricao = $dados_alteracao_viaturas ->descricao;
+                $data = $dados_alteracao_viaturas ->data;
+            } else {
+                print("<script language=JavaScript>
+                               alert('Não foi possível criar tabela.');
+                               </script>");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+         $smarty->assign('titulo', 'Atualização de Alteração de Viaturas');
+        $smarty->assign('botao', 'Atualizar');
+        $smarty->assign('evento', 'atualizar_alteracao');
+        $smarty->assign('id_alteracao_viatura', $id_alteracao_viatura);
+        $smarty->assign('id_viatura', $id_viatura);
+        $smarty->assign('odometro', $odometro);
+        $smarty->assign('descricao', $descricao);
+        $smarty->assign('data', $data);
+        $smarty->assign('relacao_viaturas', $relacao_viaturas);
+        $smarty->assign('relacao_alteracao',  $relacao_alteracao);
+        $smarty->assign('cadastrado', $_SESSION['cadastrado']);
+        $smarty->assign('atualizado', $_SESSION['atualizado']);
+        $smarty->assign('apagado', $_SESSION['apagado']);
+        $smarty->assign('login', $_SESSION['login']);
+        $smarty->display('./headers/header_datatables.tpl');
+        $smarty->display($menu);
+        $smarty->display('alteracao_viatura.tpl');
+        $smarty->display('./footer/footer_datatables.tpl');
+        unset($_SESSION['cadastrado']);
+        unset($_SESSION['atualizado']);
+        unset($_SESSION['apagado']);
+    }
+}
+?>
