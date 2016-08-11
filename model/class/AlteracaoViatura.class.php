80ebfd0ecb3ac1b1584d4263de30e79d7b8fb3f6
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-08-11 14:15:08 -0300

    Criacao

---
+++ model/class/AlteracaoViatura.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class AlteracaoViatura {
+
+    public function listarAlteracaoVtr() {
+        include '../model/conexao.php';
+        try {
+            $stmt = $pdo->prepare("SELECT id_alteracao_viatura, marcas.descricao AS marca,modelos.descricao AS  modelo,placa,alteracao_viaturas.odometro, alteracao_viaturas.descricao, DATE_FORMAT(data,'%d/%m/%Y') AS data
+                                                    FROM viaturas, marcas, modelos, alteracao_viaturas
+                                                    WHERE viaturas.id_marca = marcas.id_marca 
+                                                    AND viaturas.id_modelo = modelos.id_modelo
+                                                    AND alteracao_viaturas.id_viatura = viaturas.id_viatura");
+            $executa = $stmt->execute();
+
+            if ($executa) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                print("<script language=JavaScript>
+                         alert('Não foi possível criar tabela viaturas.');
+                         </script>");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+}
