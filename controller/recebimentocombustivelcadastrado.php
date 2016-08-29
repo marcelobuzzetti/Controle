817401d119cdd74979b76bbc88c8cb98efd6d2c4
marcelobuzzeti <marcelobuzzeti@marcelobuzzeti>
2016-08-29 09:44:52 -0300

    Alertas

--- controller/recebimentocombustivelcadastrado.php
+++ controller/recebimentocombustivelcadastrado.php
@@ -16,10 +16,18 @@ if (isset($_SESSION['login']) == FALSE || ($_SESSION['perfil'] != 1 && $_SESSION
 
     $smarty->assign('titulo', 'Recebimento de Combustíveis Cadastrados');
     $smarty->assign('relacao_rcb_combustiveis', $relacao_rcb_combustiveis);
+    $smarty->assign('cadastrado', $_SESSION['cadastrado']);
+    $smarty->assign('atualizado', $_SESSION['atualizado']);
+    $smarty->assign('apagado', $_SESSION['apagado']);
+    $smarty->assign('erro', $_SESSION['erro']);
     $smarty->assign('login', $_SESSION['login']);
     $smarty->display('./headers/header_datatables.tpl');
     $smarty->display($menu);
     $smarty->display('recebimentocombustivelcadastrado.tpl');
     $smarty->display('./footer/footer_datatables.tpl');
+    unset($_SESSION['cadastrado']);
+    unset($_SESSION['atualizado']);
+    unset($_SESSION['apagado']);
+    unset($_SESSION['erro']);
 }
 ?>
