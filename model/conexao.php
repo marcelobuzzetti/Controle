476b747f072a020eefd3f66d63aba69a2bd2a8eb
Marcelo <marcelobuzzetti@gmail.com>
2019-03-25 15:26:37 -0300

    Env

--- model/conexao.php
+++ model/conexao.php
@@ -3,7 +3,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/env.php';
 $host = $variables['DB_HOST'];
 $db = $variables['DB_DATABASE'];
 $user = $variables['DB_USERNAME'];
-$password = $variables['DB_PASSWORD'];
+$password = $variables['DB_PASSWORD'];  
 $pdo = new PDO("mysql:host=$host;dbname=$db", "$user", "$password");
 if (!$pdo) {
     die("Erro ao criar a conexão");
