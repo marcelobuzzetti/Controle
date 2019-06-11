aa769e71f76817d3a94f20f388103ff1202d4cb3
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2019-06-11 13:27:57 -0300

    API Combustivel

--- model/listaCombustivel.php
+++ model/listaCombustivel.php
@@ -3,6 +3,7 @@ include '../include/config.inc.php';
 
 $existencia_combustivel = new CombustivelExistencia();
 $tabela_existencia = $existencia_combustivel->listarCombustiveisExistentes();
+header('Access-Control-Allow-Origin: *');  
 header('Content-Type: application/json');
 echo json_encode($tabela_existencia);
 
